34c42b3c2457596c3790de44ded0a69ac639810c
user.email <mrdilmurat777@gmail.com>
2025-05-16 15:00:28 +0600

    feat: добавлена поддержка тёмной и светлой темы

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css';
 import AddList from './components/AddList';
 import AddTodoForm from './components/addTodoForm';
+import useDarkMode from './hooks/useDarkMode';
 
 function App() {
+  const { theme, toggleTheme } = useDarkMode();
 
   return (
-   <div className='max-w-xl mx-auto'>
-      <h2 className="text-2xl font-bold mb-4">📝 Todo List</h2>
-      <AddTodoForm />
-      <AddList /> 
-   </div>
+    <div className="min-h-screen bg-white dark:bg-gray-800 text-black">
+      <div className="max-w-xl mx-auto p-4 ">
+        <div className="flex justify-between items-center">
+          <h2 className={`${theme === 'dark' ? 'text-white' : 'text-black'} text-2xl font-bold mb-4`}>📝 Todo List</h2>
+          <button onClick={toggleTheme}>{theme === 'dark' ? '🌞 Светлая' : '🌚 Тёмная'}</button>
+        </div>
+        <AddTodoForm />
+        <AddList />
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // для Vite/React
   ],
+  darkMode: 'class', // включаем темную тему по классу
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
